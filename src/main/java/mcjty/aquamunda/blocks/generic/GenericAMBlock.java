e83e8958085484287d5b5b6e516a57dff33ed0d8
McJty <mcjty1@gmail.com>
2018-05-29 11:08:33 +0200

    Fixed a bug with The One Probe and WAILA integration where all information was shown twice

--- src/main/java/mcjty/aquamunda/blocks/generic/GenericAMBlock.java
+++ src/main/java/mcjty/aquamunda/blocks/generic/GenericAMBlock.java
@@ -2,29 +2,17 @@ package mcjty.aquamunda.blocks.generic;
 
 
 import mcjty.aquamunda.AquaMunda;
-import mcjty.aquamunda.compat.top.TOPInfoProvider;
 import mcjty.aquamunda.immcraft.ImmersiveCraftHandler;
-import mcjty.aquamunda.waila.WailaProvider;
 import mcjty.immcraft.api.IImmersiveCraft;
 import mcjty.immcraft.api.generic.GenericBlock;
 import mcjty.lib.McJtyRegister;
-import mcjty.theoneprobe.api.IProbeHitData;
-import mcjty.theoneprobe.api.IProbeInfo;
-import mcjty.theoneprobe.api.ProbeMode;
-import mcp.mobius.waila.api.IWailaConfigHandler;
-import mcp.mobius.waila.api.IWailaDataAccessor;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
-import java.util.List;
 import java.util.function.Function;
 
-public class GenericAMBlock extends GenericBlock implements WailaProvider, TOPInfoProvider {
+public class GenericAMBlock extends GenericBlock {
 
     public GenericAMBlock(Material material, String name, Class<? extends GenericAMTE> clazz) {
         this(material, name, clazz, ItemBlock::new);
@@ -41,15 +29,5 @@ public class GenericAMBlock extends GenericBlock implements WailaProvider, TOPIn
     @Override
     protected IImmersiveCraft getApi() {
         return ImmersiveCraftHandler.immersiveCraft;
-    }
-
-    @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-        return currenttip;
-    }
-
-    @Override
-    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
-
     }
 }
