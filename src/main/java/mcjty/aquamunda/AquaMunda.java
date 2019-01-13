461f0915dacd58eed2ad89a606ac16e1295b8aa5
McJty <mcjty1@gmail.com>
2019-01-13 09:16:44 +0100

    Fixes for latest McJtyLib and version 0.5.0

--- src/main/java/mcjty/aquamunda/AquaMunda.java
+++ src/main/java/mcjty/aquamunda/AquaMunda.java
@@ -35,9 +35,9 @@ public class AquaMunda implements ModBase {
     public static final String MODID = "aquamunda";
     public static final String MODNAME = "Aqua Munda";
     public static final String MIN_IMMCRAFT_VER = "1.5.0";
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "0.5.0";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0";
+    public static final String MIN_MCJTYLIB_VER = "3.1.0";
 
     @SidedProxy(clientSide = "mcjty.aquamunda.proxy.ClientProxy", serverSide = "mcjty.aquamunda.proxy.ServerProxy")
     public static CommonProxy proxy;
